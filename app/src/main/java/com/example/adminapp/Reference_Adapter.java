6bdd974ac37d9e79edcc762e5b00bcaeb27fa707
VedantG479 <79393956+VedantG479@users.noreply.github.com>
2023-08-13 13:01:56 +0530

    rough final

--- app/src/main/java/com/example/adminapp/Reference_Adapter.java
+++ app/src/main/java/com/example/adminapp/Reference_Adapter.java
@@ -1,12 +1,14 @@
 package com.example.adminapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.database.annotations.NotNull;
